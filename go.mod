ca098864bcbdba6d74506b09658c83c752dfe122
roi.kramer <roi.kramer@codefresh.io>
2021-02-02 10:41:11 +0200

    wip

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/go-git/go-git v4.7.0+incompatible
 	github.com/go-git/go-git/v5 v5.2.0
 	github.com/google/go-github/v32 v32.1.0
+	github.com/rhysd/go-fakeio v1.0.0
 	github.com/sirupsen/logrus v1.6.0
 	github.com/spf13/cobra v1.1.1
 	github.com/spf13/pflag v1.0.5
--- pkg/kube/kube.go
+++ pkg/kube/kube.go
@@ -2,14 +2,16 @@ package kube
 
 import (
 	"context"
+	"errors"
 	"os"
 	"path/filepath"
 	"time"
 
-	"github.com/codefresh-io/cf-argo/pkg/errors"
 	cferrors "github.com/codefresh-io/cf-argo/pkg/errors"
 	"github.com/codefresh-io/cf-argo/pkg/log"
 
+	fakeio "github.com/rhysd/go-fakeio"
+	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
@@ -41,11 +43,7 @@ type (
 
 	ApplyOptions struct {
 		// IOStreams the std streams used by the apply command
-		IOStreams genericclioptions.IOStreams
-
-		// Overwrite automatically resolve conflicts between the modified and live configuration by using values from
-		// the modified configuration
-		Overwrite bool
+		Manifests []byte
 
 		// DryRunStrategy by default false, can be set to either "client" or "server" dry-run modes, see kubectl apply --help
 		DryRunStrategy kcmdutil.DryRunStrategy
@@ -59,7 +57,7 @@ func NewConfig() *Config {
 func (c *Config) FlagSet(ctx context.Context) *pflag.FlagSet {
 	flags := pflag.NewFlagSet("kubernetes", pflag.ContinueOnError)
 
-	errors.MustContext(ctx, viper.BindEnv("kubeconfig", "KUBECONFIG"))
+	cferrors.MustContext(ctx, viper.BindEnv("kubeconfig", "KUBECONFIG"))
 	viper.SetDefault("kubeconfig", defaultConfigPath())
 
 	flags.StringVar(c.cfg.KubeConfig, "kubeconfig", viper.GetString("kubeconfig"), "path to the kubeconfig file [KUBECONFIG]")
@@ -119,7 +117,7 @@ func (c *Config) FlagSet(ctx context.Context) *pflag.FlagSet {
 // }
 
 func NewForConfig(ctx context.Context, cfg *Config) *Client {
-	var l log.Logger
+	l := log.G(ctx)
 	if *cfg.cfg.Context != "" {
 		l = l.WithField("context", *cfg.cfg.Context)
 	}
@@ -132,27 +130,72 @@ func (c *Client) Apply(ctx context.Context, opts *ApplyOptions) error {
 		return cferrors.ErrNilOpts
 	}
 
-	if opts.IOStreams.Out == nil {
-		opts.IOStreams.Out = os.Stdout
+	if opts.Manifests == nil {
+		return errors.New("no manifests")
 	}
 
-	if opts.IOStreams.ErrOut == nil {
-		opts.IOStreams.ErrOut = os.Stderr
+	applyWithTrack := ""
+	applyWithStatus := false
+	prune := false
+	ios := genericclioptions.IOStreams{
+		In:     os.Stdin,
+		Out:    os.Stdout,
+		ErrOut: os.Stderr,
 	}
 
-	applyOpts := apply.NewApplyOptions(opts.IOStreams)
-	applyOpts.Overwrite = opts.Overwrite
-	applyOpts.DryRunStrategy = opts.DryRunStrategy
-	applyOpts.PreProcessorFn = func() error {
-		c.log.Debug("running apply command")
-		return nil
-	}
-	applyOpts.PostProcessorFn = func() error {
-		c.log.Debug("finished running apply command")
-		return nil
+	o := apply.NewApplyOptions(ios)
+
+	applyCmd := &cobra.Command{
+		Use:   "apply",
+		Short: "Apply a configuration to a resource in kubernetes",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			o.DeleteFlags.FileNameFlags.Filenames = &[]string{"-"}
+			o.Overwrite = true
+			o.Prune = prune
+			o.PruneWhitelist = []string{
+				"/v1/ConfigMap",
+				"/v1/PersistentVolumeClaim",
+				"/v1/Secret",
+				"/v1/Service",
+				"/v1/ServiceAccount",
+				"apps/v1/DaemonSet",
+				"apps/v1/Deployment",
+				"batch/v1beta1/CronJob",
+				// "networking/v1/Ingress",
+			}
+			o.DryRunStrategy = opts.DryRunStrategy
+
+			if o.Namespace != "" {
+				o.EnforceNamespace = true
+			}
+
+			err := o.Complete(c, cmd)
+			if err != nil {
+				return err
+			}
+
+			fake := fakeio.StdinBytes([]byte{})
+			defer fake.Restore()
+			go func() {
+				fake.StdinBytes(opts.Manifests)
+				fake.CloseStdin()
+			}()
+
+			return o.Run()
+		},
 	}
 
-	return applyOpts.Run()
+	kcmdutil.AddDryRunFlag(applyCmd)
+	kcmdutil.AddServerSideApplyFlags(applyCmd)
+	kcmdutil.AddValidateFlags(applyCmd)
+	kcmdutil.AddFieldManagerFlagVar(applyCmd, &o.FieldManager, apply.FieldManagerClientSideApply)
+
+	applyCmd.Flags().BoolVar(&prune, "prune", false, "")
+	applyCmd.Flags().BoolVar(&applyWithStatus, "status", false, "")
+	applyCmd.Flags().StringVar(&applyWithTrack, "track", "ready", "")
+	applyCmd.SetArgs([]string{})
+
+	return applyCmd.Execute()
 }
 
 func defaultConfigPath() string {
