82dcdcaafa3b7a2d3b35c9ac27e596c436b8bf7f
roi.kramer <roi.kramer@codefresh.io>
2021-02-25 17:47:23 +0200

    wip

--- pkg/git/git_test.go
+++ pkg/git/git_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/codefresh-io/cf-argo/test/utils"
 	gg "github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
-	"github.com/go-git/go-git/v5/plumbing/transport"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -50,11 +50,11 @@ func Test_NewProvider(t *testing.T) {
 
 func Test_Clone(t *testing.T) {
 	tests := map[string]struct {
-		opts            *CloneOptions
-		expectedPath    string
-		expectedURL     string
-		expectedAuth    transport.AuthMethod
-		expectedRefName plumbing.ReferenceName
+		opts             *CloneOptions
+		expectedPath     string
+		expectedURL      string
+		expectedPassword string
+		expectedRefName  plumbing.ReferenceName
 	}{
 		"Simple": {
 			opts: &CloneOptions{
@@ -64,8 +64,40 @@ func Test_Clone(t *testing.T) {
 			},
 			expectedPath:    "/foo/bar",
 			expectedURL:     "https://github.com/foo/bar",
-			expectedAuth:    nil,
 			expectedRefName: plumbing.HEAD,
+		},
+		"With tag": {
+			opts: &CloneOptions{
+				Path: "/foo/bar",
+				URL:  "https://github.com/foo/bar@tag",
+				Auth: nil,
+			},
+			expectedPath:    "/foo/bar",
+			expectedURL:     "https://github.com/foo/bar",
+			expectedRefName: plumbing.NewTagReferenceName("tag"),
+		},
+		"With branch": {
+			opts: &CloneOptions{
+				Path: "/foo/bar",
+				URL:  "https://github.com/foo/bar#branch",
+				Auth: nil,
+			},
+			expectedPath:    "/foo/bar",
+			expectedURL:     "https://github.com/foo/bar",
+			expectedRefName: plumbing.NewBranchReferenceName("branch"),
+		},
+		"With token": {
+			opts: &CloneOptions{
+				Path: "/foo/bar",
+				URL:  "https://github.com/foo/bar",
+				Auth: &Auth{
+					Password: "password",
+				},
+			},
+			expectedPath:     "/foo/bar",
+			expectedURL:      "https://github.com/foo/bar",
+			expectedPassword: "password",
+			expectedRefName:  plumbing.HEAD,
 		},
 	}
 
@@ -76,10 +108,13 @@ func Test_Clone(t *testing.T) {
 		plainClone = func(ctx context.Context, path string, isBare bool, o *gg.CloneOptions) (*gg.Repository, error) {
 			assert.Equal(t, test.expectedPath, path)
 			assert.Equal(t, test.expectedURL, o.URL)
-			assert.Equal(t, test.expectedAuth, o.Auth)
 			assert.Equal(t, test.expectedRefName, o.ReferenceName)
 			assert.Equal(t, 1, o.Depth)
 			assert.False(t, isBare)
+			if o.Auth != nil {
+				bauth, _ := o.Auth.(*http.BasicAuth)
+				assert.Equal(t, test.expectedPassword, bauth.Password)
+			}
 
 			return nil, nil
 		}
